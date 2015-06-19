8c4d11e059edf0180e2a9a3056fd47a8783b855e
EC2 Default User <ec2-user@ip-172-31-20-178.ec2.internal>
2015-06-19 04:00:47 +0000

    correct segmentation fault, which is due to limited stack size correctly measure kenerl execution time

--- sort/sort.cu
+++ sort/sort.cu
@@ -1,11 +1,11 @@
 #include<time.h>
-#include<iostream>
 #include<stdlib.h>
+#include<stdio.h>
 
-#define NUM_VECS (1024 )
+#define NUM_VECS (1024*128)
 #define VEC_LENGTH 512
 #define TOTAL_SIZE NUM_VECS*VEC_LENGTH
-#define NUM_BLOCKS 2 
+#define NUM_BLOCKS 256 
 #define NUM_THREADS NUM_VECS/NUM_BLOCKS
 
 using namespace std;
@@ -39,8 +39,8 @@ void sort_cpu(float* vecs, unsigned int length, unsigned int num_vecs){
 }
 
 int main(void){
-    float vecs[TOTAL_SIZE];
-    float vecs_sorted_by_gpu[TOTAL_SIZE];
+    float* vecs = new float[TOTAL_SIZE];
+    float* vecs_sorted_by_gpu = new float[TOTAL_SIZE];
 
     srand(time(NULL));
     for(int i=0; i<TOTAL_SIZE; i++){
@@ -48,32 +48,61 @@ int main(void){
     }
 
     float* dev_vecs;
-    cudaMalloc((void**) & dev_vecs, TOTAL_SIZE*sizeof(float));
+    cudaError_t error;
+    error = cudaMalloc((void**) & dev_vecs, TOTAL_SIZE*sizeof(float));
+    printf("allocation mem on device: %s\n", cudaGetErrorString(error));
     cudaMemcpy(dev_vecs, vecs, TOTAL_SIZE*sizeof(float), cudaMemcpyHostToDevice);
 
     clock_t t1 = clock();
-    sort_gpu<<<NUM_BLOCKS, NUM_THREADS>>>(dev_vecs, VEC_LENGTH);
-    clock_t t2 = clock();
-    cout << "GPU time = " << ((double)(t2-t1)) / CLOCKS_PER_SEC << '\n';
+    cudaEvent_t start,stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
 
+    cudaEventRecord(start, 0);
+
+    sort_gpu<<<NUM_BLOCKS, NUM_THREADS>>>(dev_vecs, VEC_LENGTH);
+
+    cudaEventRecord(stop, 0); 
+    cudaEventSynchronize(stop);
+    float elapsedTime; 
+    cudaEventElapsedTime(&elapsedTime, start, stop); 
+    printf("time required : %f ms", elapsedTime);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+
+    cudaMemcpy(vecs_sorted_by_gpu, dev_vecs, TOTAL_SIZE*sizeof(float), cudaMemcpyDeviceToHost);
+
+    size_t free, total;
+    printf("\n");
+    cudaMemGetInfo(&free,&total); 
+    printf("%4f MB free of total %4f MB\n",(float)free/1024./1024.,total/1024./1024.);
+
+    /*
     clock_t t3 = clock();
     sort_cpu(vecs, VEC_LENGTH, NUM_VECS);
     clock_t t4 = clock();
-    cout << "CPU time = " << ((double)(t4-t3)) / CLOCKS_PER_SEC << '\n';
+    printf("CPU time = %f (ms)", 1000.*((double)(t4-t3)) / CLOCKS_PER_SEC);
 
-    cudaMemcpy(vecs_sorted_by_gpu, dev_vecs, TOTAL_SIZE*sizeof(float), cudaMemcpyDeviceToHost);
 
     char pass_test = 1;
     for(int i=0; i<NUM_VECS; i++){
         for(int j=0; j<VEC_LENGTH; j++){
             if(vecs_sorted_by_gpu[i*VEC_LENGTH+j] != vecs[i*VEC_LENGTH+j]){
-                cout << "GPU and CPU results differs at " << i*VEC_LENGTH+j;
+                printf("GPU and CPU results differs at %d", i*VEC_LENGTH+j);
                 pass_test = 0;
             }
         }
     }
 
     if(pass_test){
-        cout << "GPU and CPU yeild the same result\n";
+        printf("GPU and CPU yeild the same result\n");
+    }
+    */
+
+    for(int i=0; i<NUM_VECS; i++){
+        for(int j=0; j<VEC_LENGTH; j++){
+            printf("%4f, ", vecs_sorted_by_gpu[i*VEC_LENGTH+j]);
+            }
+        printf("\n\n");
     }
 }
